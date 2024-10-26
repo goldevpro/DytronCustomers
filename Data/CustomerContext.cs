89a6a6ec8fcb9ec9319790b096ddbb5122eefc58
ne15813@dow.com <ne15813@dow.com>
2024-10-26 03:45:14 -0600

    feature: add seed customers with ef core

--- Data/CustomerContext.cs
+++ Data/CustomerContext.cs
@@ -7,5 +7,20 @@ namespace DytronCustomers.Data
     {
         public CustomerContext(DbContextOptions<CustomerContext> options) : base(options){}
         public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Customer>().HasData(
+                Enumerable.Range(1, 20).Select(i => new Customer
+                {
+                    FirstName = $"First-{i}",
+                    LastName = $"Last-{i}",
+                    Email = $"customer{i}@example.com",
+                    CreatedDate = DateTime.Now,
+                    LastUpdatedDate = DateTime.Now
+                }).ToArray()
+                );
+        }
     }
 }
